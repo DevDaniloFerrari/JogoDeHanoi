4cc289bcccb0a3f0174098eae82366c7dde94f08
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2019-05-09 00:23:13 -0300

    Adicionando e implementando classe JogoDeHanoi

---
+++ src/jogo/JogoDeHanoi.java
@@ -0,0 +1,75 @@
+package jogo;
+
+import pilha.estatica.classes.PilhaEstatica;
+import pilha.estatica.interfaces.IPilhaEstatica;
+
+public class JogoDeHanoi {
+
+    private final IPilhaEstatica PilhaInicial, PilhaUm, PilhaDois;
+    private int NumeroDeJogadas;
+    private int TamanhoDasPilhas;
+
+    public JogoDeHanoi(int tamanhoDasPilhas) throws Exception {
+        this.TamanhoDasPilhas = tamanhoDasPilhas;
+        this.PilhaInicial = new PilhaEstatica(tamanhoDasPilhas);
+
+        for (int i = tamanhoDasPilhas; i > 0; i--) {
+            this.PilhaInicial.push(i);
+        }
+
+        this.PilhaUm = new PilhaEstatica(tamanhoDasPilhas);
+        this.PilhaDois = new PilhaEstatica(tamanhoDasPilhas);
+
+        this.NumeroDeJogadas = 0;
+    }
+
+    public boolean fazerJogada(int numeroNaTorreDeOrigem, int numeroNaTorreDeDestino) throws Exception {
+        IPilhaEstatica pilhaDeOrigem = this.obterTorre(numeroNaTorreDeOrigem);
+        IPilhaEstatica pilhaDeDestino = this.obterTorre(numeroNaTorreDeDestino);
+        
+        if(pilhaDeOrigem==null || pilhaDeDestino==null){
+            return false;
+        }
+        
+        if(pilhaDeOrigem.isEmpty()){
+            return false;
+        }
+        
+        if(pilhaDeDestino.isFull()){
+            return false;
+        }
+        
+        if((int)pilhaDeOrigem.peek()>(int)pilhaDeDestino.peek()){
+            return false;
+        }
+        
+        pilhaDeDestino.push(pilhaDeOrigem.pop());
+        this.NumeroDeJogadas++;
+        
+        return true;
+    }
+
+    private IPilhaEstatica obterTorre(int topo) throws Exception {
+
+        if (topo < 0 || topo > this.TamanhoDasPilhas) {
+            return null;
+        }
+        
+        if(!this.PilhaInicial.isEmpty()){
+            if(topo == (int)this.PilhaInicial.peek())
+                return this.PilhaInicial;
+        }
+        
+        if(!this.PilhaUm.isEmpty()){
+            if(topo == (int)this.PilhaUm.peek())
+                return this.PilhaUm;
+        }
+        
+        if(!this.PilhaDois.isEmpty()){
+            if(topo == (int)this.PilhaDois.peek())
+                return this.PilhaDois;
+        }
+        
+        return null;
+    }
+}
