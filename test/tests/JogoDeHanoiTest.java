628fde15edc63c4b545fe60a85616f69d5a06cc0
manoelmerlin <manoelmerlin@hotmail.com>
2019-05-19 18:27:00 -0300

    Teste de jogada terminada

--- test/tests/JogoDeHanoiTest.java
+++ test/tests/JogoDeHanoiTest.java
@@ -23,6 +23,43 @@ public class JogoDeHanoiTest {
     public void testFazerJogadaComOrigemEDestinoInvalidos() throws Exception {
         Assert.assertFalse(jogoDeHanoi.fazerJogada(2, 1));
     }
-    
-    
+
+    @Test
+    public void testTerminarJogada() throws Exception {
+        System.out.println(jogoDeHanoi.imprimir());
+        jogoDeHanoi.fazerJogada(1, 2);
+        jogoDeHanoi.fazerJogada(1, 3);
+        jogoDeHanoi.fazerJogada(2, 3);
+        jogoDeHanoi.fazerJogada(1, 2);
+        jogoDeHanoi.fazerJogada(3, 2);
+        jogoDeHanoi.fazerJogada(3, 1);
+        jogoDeHanoi.fazerJogada(2, 3);
+        jogoDeHanoi.fazerJogada(1, 2);
+        jogoDeHanoi.fazerJogada(3, 2);
+        jogoDeHanoi.fazerJogada(1, 3);
+        jogoDeHanoi.fazerJogada(2, 1);
+        jogoDeHanoi.fazerJogada(2, 3);
+        jogoDeHanoi.fazerJogada(1, 2);
+        jogoDeHanoi.fazerJogada(3, 1);
+        jogoDeHanoi.fazerJogada(2, 1);
+        jogoDeHanoi.fazerJogada(2, 3);
+        jogoDeHanoi.fazerJogada(1, 2);
+        jogoDeHanoi.fazerJogada(1, 3);
+        jogoDeHanoi.fazerJogada(2, 3);
+
+        System.out.println(jogoDeHanoi.imprimir());
+
+        Assert.assertEquals(true, jogoDeHanoi.terminou());
+
+    }
+
+    @Test
+    public void testNumeroDeJogadas() throws Exception {
+        jogoDeHanoi.fazerJogada(1, 2);
+        jogoDeHanoi.fazerJogada(1, 3);
+        jogoDeHanoi.fazerJogada(2, 3);
+
+        Assert.assertEquals(3, jogoDeHanoi.getNumeroJogadas());
+    }
+
 }
